7de80dcea0c9cecdb539c3ed77161fc2512e6322
Your Name <you@example.com>
2023-05-05 13:55:11 -0500

    bind port 9000 onwards

--- src/main/java/com/smartbros/Application.java
+++ src/main/java/com/smartbros/Application.java
@@ -3,20 +3,21 @@ package com.smartbros;
 import io.micronaut.runtime.Micronaut;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         int port = NetworkUtils.getAvailablePort();
-        Set<String> externalIps = NetworkUtils.getExternalIps();
+        List<String> externalIps = NetworkUtils.getExternalIps();
 
         System.out.println("Server is listening on the following IP addresses:");
         for (String ip : externalIps) {
             System.out.println("- " + ip + ":" + port);
         }
 
-        Micronaut.run(Application.class, new String[]{"--server.port=" + port});
+        Micronaut.run(Application.class, new String[]{"-Dmicronaut.server.port=" + port});
 
     }
 }
--- src/main/java/com/smartbros/NetworkUtils.java
+++ src/main/java/com/smartbros/NetworkUtils.java
@@ -1,36 +1,52 @@
 package com.smartbros;
 
 import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.SocketException;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Set;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class NetworkUtils {
-
-    public static int getAvailablePort() throws IOException {
-        try (ServerSocket socket = new ServerSocket(0)) {
-            return socket.getLocalPort();
+    public static int getAvailablePort() throws Exception {
+        for (int port = 9000; port < 65535; port++) {
+            try (Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress("0.0.0.0", port), 1);
+            } catch (IOException e) {
+                return port;
+            }
         }
+        throw new Exception("No available port found");
     }
 
-    public static Set<String> getExternalIps() throws SocketException {
-        Set<String> ips = new HashSet<>();
+    public static List<String> getExternalIps() throws SocketException {
+        List<String> externalIPs = new ArrayList<>();
         Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
         while (networkInterfaces.hasMoreElements()) {
             NetworkInterface networkInterface = networkInterfaces.nextElement();
             Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+
             while (inetAddresses.hasMoreElements()) {
                 InetAddress inetAddress = inetAddresses.nextElement();
-                if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
-                    ips.add(inetAddress.getHostAddress());
+                String ipAddress = inetAddress.getHostAddress();
+
+                if (ipAddress.contains(".") && !ipAddress.startsWith("127.") && !ipAddress.startsWith("0.")) {
+                    externalIPs.add(ipAddress);
                 }
             }
         }
-        return ips;
+        return externalIPs;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int availablePort = getAvailablePort();
+            System.out.println("Available port: " + availablePort);
+
+            List<String> externalIPs = getExternalIps();
+            System.out.println("External IPs: " + externalIPs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
